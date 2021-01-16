51f89b4a7da03989a2f06e5bd9e430cb33c0b718
Your Name <you@example.com>
2021-01-16 15:48:58 -0600

    Implemented a moduler routing technique.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 //You need to type "npm run rishav" not "npm rishav"
 const express = require("express");
+const root = require("./routes/root");
 const app = express();
 const port = 5000;
-app.get("/", (req,res) => {
-  res.json(`Welcome to the GeeksforGeeks API server.`);
-});
+const todos = ["Milk","Curd","Sugar"];
+
+app.use("/",root);
+
 app.listen(port,() => {
   console.log(`Server started at port ${port}.`);
 });
