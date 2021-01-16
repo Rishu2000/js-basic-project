a5d49c876b1b178d92bbf4774dda47e20fb21447
Your Name <you@example.com>
2021-01-16 08:41:00 -0600

    Send json type in responce.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 app.get("/", (req,res) => {
-  res.send("Hello,GFG!");
+  res.json(`Hello,GFG!`);
 });
 app.listen(port,() => {
   console.log(`Server started at port ${port}.`);
