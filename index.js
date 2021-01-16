99cc035b52faa648829586d18df2c4601f86f2de
Your Name <you@example.com>
2021-01-16 08:47:52 -0600

    Created a basic Express JS Web Server.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 app.get("/", (req,res) => {
-  res.json(`Hello,GFG!`);
+  res.json(`Hello,GeeksforGeeks!`);
 });
 app.listen(port,() => {
   console.log(`Server started at port ${port}.`);
