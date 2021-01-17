ee75d0d9dee726b24667993d6cbc4e082a186511
Your Name <you@example.com>
2021-01-16 18:04:40 -0600

    Added some constants.

---
+++ constants/users.js
@@ -0,0 +1,27 @@
+module.exports =  [
+  {
+    Name: "Praveen Kumar",
+    Password:"Hello@123",
+    LinkedIn:"https://www.linkedin.com/p"
+  },
+  {
+    Name: "Ashish Sharma",
+    Password:"!@344443",
+    LinkedIn:"https://www.linkedin.com/a"
+  },
+  {
+    Name: "Sanskar",
+    Password:"Yooo",
+    LinkedIn:"https://www.linkedin.com/s"
+  },
+  {
+    Name: "Himadri Bhargava",
+    Password:"himadri@123",
+    LinkedIn:"https://www.linkedin.com/h"
+  },
+  {
+    Name: "Raju",
+    Password:"pass@123",
+    LinkedIn:"https://www.linkedin.com/r"
+  }
+]
