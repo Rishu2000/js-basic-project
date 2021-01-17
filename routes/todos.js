4609e59b8ed065edefe371f99b4ed7034a6e6496
Your Name <you@example.com>
2021-01-16 18:31:19 -0600

    Changed routes and included constants.

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const todos = require("../constants/todos")
 const app = express.Router();
 
 app.get("/",(req,res) => {
-  res.json("Welcome to todos.");
+  res.json(todos);
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const users = require("../constants/users");
 const app = express.Router();
 
 app.get("/",(req,res) => {
-  res.json("Welcome to Users.");
+  res.json(users);
+  //res.json(users);
 });
 
 module.exports = app;
