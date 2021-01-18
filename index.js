6412dcbec72b7a370b98dc80d79b0fd81e6e4017
Your Name <you@example.com>
2021-01-17 20:55:05 -0600

    Implemented morgan middleware.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 //You need to type "npm run rishav" not "npm rishav"
 const express = require("express");
+const morgan = require("morgan");
 const root = require("./routes/root");
 const app = express();
 const port = 5000;
 const todos = ["Milk","Curd","Sugar"];
 
+app.use(morgan("dev"));
 app.use("/",root);
 
 app.listen(port,() => {
