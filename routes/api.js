2190096b09dfe3936224473dd786fe0ae2ebe971
Your Name <you@example.com>
2021-01-17 21:59:56 -0600

    Funny different massages between Browsers and Postman.

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,11 @@ const todos = require("./todos");
 const app = express.Router();
 
 app.get("/",(req,res) => {
+  if (req.headers["user-agent"].toLowerCase().indexOf("postman")>-1) {
+    res.json("Hey, there Developer.")
+  }else{
   res.json("Welcome to API.");
+}
 });
 app.use("/users/",users);
 app.use("/todos/",todos);
