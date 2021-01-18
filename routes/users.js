b8f115a4b167886de41d6c40d2cfab898d41f936
Your Name <you@example.com>
2021-01-17 18:57:53 -0600

    Implemented userID and make Password invisible to show.

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,21 @@ const users = require("../constants/users");
 const app = express.Router();
 
 app.get("/",(req,res) => {
-  res.json(users);
-  //res.json(users);
+  res.json(users.map((n,userID) => {
+    const a = {userID,...n};
+    delete a.Password;
+    return a;
+  }));
+});
+app.get("/:id",(req,res) => {
+  const userID = +req.params.id;
+  if (users[userID]) {
+    const a = {userID,...users[userID]};
+    delete a.Password;
+    res.json(a);
+  }else{
+    res.status(404).json("Error, Can not find User.")
+  }
 });
 
 module.exports = app;
