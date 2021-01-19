3182c4e499bf35557e2b315ad788795a7248f3f9
Your Name <you@example.com>
2021-01-19 14:51:24 -0600

    Fixed the logout functionality.

--- routes/users.js
+++ routes/users.js
@@ -71,7 +71,7 @@ app.post("/login",(req,res) => {
   }
 });
 app.post("/logout",(req,res) => {
-  res.destroy();
+  req.session.destroy();
   res.json({Success:true});
 });
 app.get("/:id",(req,res) => {
