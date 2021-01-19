d94d45165dc158edf8d8e821ee2678064b87b628
Your Name <you@example.com>
2021-01-19 10:48:40 -0600

    Single client authentication implemented and NOTE Authentcation declared with LET*.

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const users = require("../constants/users");
 const app = express.Router();
+let Authentacation = true;
+
+app.use(express.json());
 
 app.get("/",(req,res) => {
   res.json(users.map((n,userID) => {
@@ -8,6 +11,27 @@ app.get("/",(req,res) => {
     delete a.Password;
     return a;
   }));
+});
+app.get("/login",(req,res) => {
+  console.log("get");
+  res.json({Authentacation});
+});
+app.post("/login",(req,res) => {
+  const {Username,Password} = req.body;
+  console.log("post");
+  if (!Username || !Password) {
+    //res.json({Username,Password});
+    res.status(400).json("Please enter both.");
+  }else{
+    const matched = users.filter(u => u.Username.toLowerCase() === Username.toLowerCase() && u.Password === Password);
+    if (matched.length === 1) {
+      Authentacation = true;
+      res.json({Success : true});
+    }else if (matched.length === 0) {
+      Authentacation = false;
+      res.status(401).json("Oops Bad Credentials");
+    }
+  }
 });
 app.get("/:id",(req,res) => {
   const userID = +req.params.id;
